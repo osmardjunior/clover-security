c6fd48fa3ef6a0323381737035cafe22f88a8510
cindyhanae <cinde01@gmail.com>
2021-12-13 19:05:08 -0300

    dropdown

--- js/app.js
+++ js/app.js
@@ -14,4 +14,23 @@ function menuHandler(event){
 window.addEventListener('scroll', menuHandler)
 
 // OPEN/CLOSE DROPDOWN
-const 
+const menu1DropdownHandler = document.querySelector('.js-top-dropdown_menu1')
+const menu1 = document.querySelector('.menu-dropdown1')
+const menu2DropdownHandler = document.querySelector('.js-top-dropdown_menu2')
+const menu2 = document.querySelector('.menu-dropdown2')
+
+
+function dropdownMenu1() {
+  menu1.classList.toggle('active')
+  menu2.classList.remove('active')
+}
+
+function dropdownMenu2() {
+  menu2.classList.toggle('active')
+  menu1.classList.remove('active')
+}
+
+menu1DropdownHandler.addEventListener('click', dropdownMenu1)
+menu2DropdownHandler.addEventListener('click', dropdownMenu2)
+menu1.addEventListener('mouseleave', dropdownMenu1)
+menu2.addEventListener('mouseleave', dropdownMenu2)
