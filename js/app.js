ff79133c6bbc50a9163336bf0ce8c7e1514294c1
cindyhanae <cinde01@gmail.com>
2021-12-13 19:51:13 -0300

    align dropdown

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // CHANGE MENU ON SCROLL
+const header = document.querySelector('.js-header')
+
 function menuHandler(event){
-  const header = document.querySelector('.js-header')
 
   if (window.scrollY > 200) {
     header.classList.add('active')
@@ -20,16 +21,25 @@ const menu2DropdownHandler = document.querySelector('.js-top-dropdown_menu2')
 const menu2 = document.querySelector('.menu-dropdown2')
 
 
+
 function dropdownMenu1(event) {
   event.preventDefault()
   menu1.classList.toggle('active')
   menu2.classList.remove('active')
+
+  if (header.classList.contains('active')) {
+    menu1.style.top = '7.1rem'
+  }
 }
 
 function dropdownMenu2(event) {
   event.preventDefault()
   menu2.classList.toggle('active')
   menu1.classList.remove('active')
+
+  if (header.classList.contains('active')) {
+    menu2.style.top = '7.1rem'
+  }
 }
 
 menu1DropdownHandler.addEventListener('click', dropdownMenu1)
