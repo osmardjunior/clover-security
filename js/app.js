5067fd38220358d20ae3043e24216745f725b42f
cindyhanae <cinde01@gmail.com>
2021-12-14 17:11:39 -0300

    footer responsive

--- js/app.js
+++ js/app.js
@@ -18,13 +18,12 @@ function menuHandler(event){
 
 window.addEventListener('scroll', menuHandler)
 
+
 // OPEN/CLOSE DROPDOWN
 const menu1DropdownHandler = document.querySelector('.js-top-dropdown_menu1')
 const menu1 = document.querySelector('.menu-dropdown1')
 const menu2DropdownHandler = document.querySelector('.js-top-dropdown_menu2')
 const menu2 = document.querySelector('.menu-dropdown2')
-
-
 
 function dropdownMenu1(event) {
   event.preventDefault()
@@ -32,17 +31,34 @@ function dropdownMenu1(event) {
   menu1.classList.toggle('active')
   menu2.classList.remove('active')
 
-  
 }
 
 function dropdownMenu2(event) {
   event.preventDefault()
   menu2.classList.toggle('active')
   menu1.classList.remove('active')
-  
 }
 
 menu1DropdownHandler.addEventListener('click', dropdownMenu1)
 menu2DropdownHandler.addEventListener('click', dropdownMenu2)
-menu1.addEventListener('mouseleave', dropdownMenu1)
-menu2.addEventListener('mouseleave', dropdownMenu2)
+// menu1.addEventListener('mouseleave', dropdownMenu1)
+// menu2.addEventListener('mouseleave', dropdownMenu2)
+
+
+// ACCORDION MOBILE FOOTER
+const accordionContent = document.querySelectorAll('.js-accordion')
+const allAccordions = document.querySelectorAll('.accordion-footer')
+
+accordionContent.forEach((item) => {
+    function accordionActive() {
+      allAccordions.forEach((accordion) => {
+        accordion.classList.remove('active')
+      })
+
+        this.parentElement.classList.add('active')
+
+    }
+
+    item.addEventListener('click', accordionActive)
+})
+
