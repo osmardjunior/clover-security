456e136feaeb000cdd93d7fedd08cde2e843575b
cindyhanae <cinde01@gmail.com>
2021-12-27 16:35:46 -0300

    update dropdown list

--- js/app.js
+++ js/app.js
@@ -1,66 +1,4 @@
 // CHANGE MENU ON SCROLL
-// const header = document.querySelector('.js-header')
-// const menuDropdown = document.querySelector('.js-menu-dropdown')
-
-// function menuHandler(){
-
-//   if (window.scrollY > 200 && window.innerWidth > 950) {
-//     header.classList.add('active')
-//     menuDropdown.style.top = '7.4rem'
-
-//   } else if(window.scrollY < 200 && window.innerWidth > 950){
-//     header.classList.remove('active')
-//     menuDropdown.style.top = '14rem'
-
-//   } else if(window.scrollY < 200 && window.innerWidth < 950){
-//     header.classList.remove('active')
-//     menuDropdown.style.top = '11.1rem'
-
-//   }
-// }
-
-// window.addEventListener('scroll', menuHandler)
-
-// OPEN/CLOSE DROPDOWN
-// const menu1 = document.querySelector('.menu-dropdown1')
-// const menu2 = document.querySelector('.menu-dropdown2')
-// const menu1DropdownHandler = document.querySelector('.js-top-dropdown_menu1')
-// const menu2DropdownHandler = document.querySelector('.js-top-dropdown_menu2')
-// const body = document.querySelector('body')
-
-
-// function handleDropdownMenu1(event) {
-//   event.preventDefault()
-
-//   menu1.classList.toggle('active')
-//   menu2.classList.remove('active')
-//   body.classList.toggle('prevent-scroll')
-
-//   if(body.classList.contains('js-prevent-scroll')){
-//     body.classList.remove('js-prevent-scroll')
-//   }
-  
-// // }
-
-// function handleDropdownMenu2(event) {
-//   event.preventDefault()
-
-//   menu2.classList.toggle('active')
-//   menu1.classList.remove('active')
-//   body.classList.toggle('prevent-scroll')
-  
-//   if(body.classList.contains('js-prevent-scroll')){
-//     body.classList.remove('js-prevent-scroll')
-//   }
-// }
-
-// menu1DropdownHandler.addEventListener('click', handleDropdownMenu1)
-// menu2DropdownHandler.addEventListener('click', handleDropdownMenu2)
-// menu1.addEventListener('mouseleave', handleDropdownMenu1)
-// menu2.addEventListener('mouseleave', handleDropdownMenu2)
-
-
-// CHANGE MENU ON SCROLL
 const header = document.querySelector('.js-header')
 
 function menuHandler(){
@@ -108,7 +46,7 @@ allHeaderLinks.forEach((item) => {
     })
     
     overlay.style.display = 'block'
-    // preventScroll.classList.add('noscroll')
+    preventScroll.classList.add('noscroll')
 
     // allHeaderLinks.forEach((menudropdown) => {
     //   if(menudropdown.classList.contains('active')){
@@ -134,11 +72,11 @@ allMenuDropdown.forEach((menu) => {
   const activeDropdown = document.querySelector('.js-menu-dropdown.active')
 
   overlay.style.display = 'none'
-  // preventScroll.classList.remove('noscroll')
+  preventScroll.classList.remove('noscroll')
   activeDropdown.classList.remove('active')
   
   }
-  // menu.addEventListener('mouseleave', mouseLeaveDrodpown)
+  menu.addEventListener('mouseleave', mouseLeaveDrodpown)
 })
 
 
