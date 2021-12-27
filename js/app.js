7144e59e92ae698408e824467bc9517ecd2c56b4
cindyhanae <cinde01@gmail.com>
2021-12-27 13:42:26 -0300

    overlay and background fixed when dropdown open

--- js/app.js
+++ js/app.js
@@ -80,9 +80,12 @@ function menuHandler(){
 window.addEventListener('scroll', menuHandler)
 window.addEventListener('resize', menuHandler)
 
+
 // OPEN CLOSE HEADER DROPDOWN
 const allHeaderLinks = document.querySelectorAll('.js-menu-dropdown')
 const allMenuDropdown = document.querySelectorAll('.menu-dropdown')
+const overlay = document.querySelector('.overlay')
+const preventScroll = document.querySelector('main')
 
 allHeaderLinks.forEach((item) => {
   
@@ -90,19 +93,31 @@ allHeaderLinks.forEach((item) => {
     const activeDropdown = document.querySelector('.js-menu-dropdown.active')
 
     event.preventDefault()
-
+    
     allMenuDropdown.forEach((menu) => {
       if (window.scrollY > 200 && window.innerWidth > 1200) {
         menu.style.top = '6.9rem'
-    
+        
       } else if(window.scrollY < 200 && window.innerWidth > 1200){
         menu.style.top = '14rem'
-    
+        
       } else if(window.innerWidth < 1200){
         menu.style.top = '11.1rem'
-    
+        
       }
     })
+    
+    overlay.style.display = 'block'
+    preventScroll.classList.add('noscroll')
+
+    // allHeaderLinks.forEach((menudropdown) => {
+    //   if(menudropdown.classList.contains('active')){
+    //     overlay.style.display = 'block'
+
+    //   } else{
+    //     overlay.style.display = 'none'
+    //   }
+    // })
 
     this.classList.add('active')
     activeDropdown.classList.remove('active')
@@ -111,13 +126,19 @@ allHeaderLinks.forEach((item) => {
   item.addEventListener('click', dropdownHandler)
 })
 
+
+
 // OPEN/CLOSE DROPDOWN ON MOUSE LEAVE
+allMenuDropdown.forEach((menu) => {
+  function mouseLeaveDrodpown(){
+  const activeDropdown = document.querySelector('.js-menu-dropdown.active')
 
-// allMenuDropdown.forEach((menu) => {
-//   function mouseLeaveDrodpown(){
-//   const activeDropdown = document.querySelector('.js-menu-dropdown.active')
+  overlay.style.display = 'none'
+  preventScroll.classList.remove('noscroll')
+  activeDropdown.classList.remove('active')
+  
+  }
+  menu.addEventListener('mouseleave', mouseLeaveDrodpown)
+})
 
-//   activeDropdown.classList.remove('active')
-//   }
-//   menu.addEventListener('mouseleave', mouseLeaveDrodpown)
-// })
+
