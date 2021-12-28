4df74ee1b72e48de1f92c9eb81fb8c0962579754
cindyhanae <cinde01@gmail.com>
2021-12-28 02:28:15 -0300

    update

--- js/app.js
+++ js/app.js
@@ -24,53 +24,8 @@ const allHeaderLinks = document.querySelectorAll('.js-menu-dropdown')
 const allMenuDropdown = document.querySelectorAll('.menu-dropdown')
 const overlay = document.querySelector('.overlay')
 const preventScroll = document.querySelector('main')
-const pseudoElem = document.querySelector('nav.menu .nav-menu  li.active > a::after')
 
-// allHeaderLinks.forEach((item) => {
-  
-//   function dropdownHandler(event){
-//     const activeDropdown = document.querySelector('.js-menu-dropdown.active')
-
-//     // event.preventDefault()
-    
-//     allMenuDropdown.forEach((menu) => {
-//       if (window.scrollY > 200 && window.innerWidth > 1200) {
-//         menu.style.top = '6.9rem'
-        
-//       } else if(window.scrollY < 200 && window.innerWidth > 1200){
-//         menu.style.top = '14rem'
-        
-//       } else if(window.innerWidth < 1200){
-//         menu.style.top = '11.1rem'
-        
-//       }
-//     })
-    
-//     overlay.style.display = 'block'
-//     preventScroll.classList.add('noscroll') 
-//     this.classList.add('active')
-//     // pseudoElem.style.width = "100%"
-//     activeDropdown.classList.remove('active')
-//   }
-
-//   item.addEventListener('mouseover', dropdownHandler)
-// })
-
-
-// CLOSE DROPDOWN ON MOUSE LEAVE
-allMenuDropdown.forEach((menu) => {
-  function mouseLeaveDrodpown(){
-  const activeDropdown = document.querySelector('.js-menu-dropdown.active')
-
-  overlay.style.display = 'none'
-  preventScroll.classList.remove('noscroll')
-  activeDropdown.classList.remove('active')
-  // pseudoElem.style.width = "0"
-  
-  }
-  menu.addEventListener('mouseout', mouseLeaveDrodpown)
-})
-
+// OPEN MENU1 / MENU2 ON MOUSEOVER
 const menu1 = document.querySelector('.js-menu-dropdown1')
 const menu2 = document.querySelector('.js-menu-dropdown2')
 
@@ -89,3 +44,17 @@ function handleDropdown2(){
   menu1.classList.remove('active')
 }
 menu2.addEventListener('mouseover', handleDropdown2)
+
+// CLOSE DROPDOWN ON MOUSE LEAVE
+// allMenuDropdown.forEach((menu) => {
+//   function mouseLeaveDrodpown(){
+//   const activeDropdown = document.querySelector('.js-menu-dropdown.active')
+
+//   overlay.style.display = 'none'
+//   preventScroll.classList.remove('noscroll')
+//   activeDropdown.classList.remove('active')
+//   // pseudoElem.style.width = "0"
+  
+//   }
+//   menu.addEventListener('mouseout', mouseLeaveDrodpown)
+// })
